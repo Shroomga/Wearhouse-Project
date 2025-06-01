5d97c2e270dd729baf31d52ea0fad1c260ebb2a4
shroom-test <shroomga@gmail.com>
2025-06-01 08:25:45 +0200

    Edit buttons, databases and transition screens

---
+++ views/seller/add-item.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <select name="category">
+            <option value="shirts">Shirts</option>
+            <option value="trousers">Trousers</option>
+            <option value="hatsAndAccessories">Hats and Accessories</option>
+            <option value="jackets">Jackets</option>
+            <option value="blouses">Blouses</option>
+            <option value="shoes">Shoes</option>
+        </select>
+        <input type="text" name="description">
+        <input type="submit" value="Add Item">
+    </form>
+</body>
+</html>
