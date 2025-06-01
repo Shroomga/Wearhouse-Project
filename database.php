8bb7e134ddadca366df3146b0e42778cd0e1792b
shroom-test <shroomga@gmail.com>
2025-06-01 08:26:52 +0200

    Edit buttons, databases and transition screens

--- database.php
+++ database.php
@@ -1,3 +1,14 @@
 <?php
     //id, name, price, details, image
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "wearhousedb";
+    $conn = "";
+
+    try {
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    } catch (mysqli_sql_exception) {
+        echo"<p>Could not connect!</p>";
+    }
 ?>
--- views/auth.php
+++ views/auth.php
@@ -2,6 +2,8 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
     if(!empty($_POST["register"])){
+        $confirm = $_POST["confirm"];
+        
         header("./views/login.php");
     }
     if(!empty($_POST["login"])){
