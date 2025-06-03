d3a2e9fdd32c7475eb614234a69781a5b04b44a4
shroom-test <shroomga@gmail.com>
2025-06-03 23:11:44 +0200

    Add code for displaying all items when no category is selected in store.php

--- views/store.php
+++ views/store.php
@@ -1,5 +1,49 @@
 <?php
     include("../database.php");
+    class Item {
+        protected $name;
+        protected $category;
+        protected $description;
+        protected $price;
+        
+
+        function __construct($name, $category, $description, $price)
+        {
+            $this->name = $name;
+            $this->category = $category;
+            $this->description = $description;
+            $this->price = $price;
+            
+        }
+        function get_name() {
+        return $this->name;
+        }
+        function get_category() {
+        return $this->category;
+        }
+        function get_description(){
+            return $this->description;
+        }
+        function get_price() {
+        return $this->price;
+        }
+        
+    }
+    $items = [];
+    try {
+        $result = mysqli_query($conn, "SELECT * FROM items");
+        if(mysqli_num_rows($result) > 0){
+            while ($row = mysqli_fetch_assoc($result)){
+                $name = $row["name"];
+                $category = $row["category"];
+                $description = $row["description"];
+                $price = $row["price"];
+                $items[] = new Item($name, $category, $description, $price);
+            }
+        }
+    } catch (mysqli_sql_exception) {
+        echo "SQL Query error encountered";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -21,16 +65,18 @@
         <!-- If no category was selected, show all items. 
         If a category was selected, display those category items. -->
         <div>
+            <?php foreach ($items as $item){ ?>
             <div>
                 <form>
                 <img src="someimg.png" alt="some image">
-                <p>Item 1</p>
-                <p>Price 1</p>
-                <p>Details 1</p>
+                <h2><?php echo $item->get_name();?></h2>
+                <p><?php echo $item->get_price();?></p>
+                <p><?php echo $item->get_description();?></p>
                 <input type="submit" name="add" value="Add to Cart">
                 </form>
                 <p><a href="./cart.php">Go to Cart</a></p>
             </div>
+            <?php } ?>
         </div>
     </div>
 </body>
